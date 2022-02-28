103101be6a4858f444a143c5d6e43f972903545a
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-02-28 11:32:46 +0900

    Update tapping_keypads.py

--- tapping_keypads.py
+++ tapping_keypads.py
@@ -38,9 +38,9 @@ Constraints:
 
 Example
 numbers                             hand	    result
-[1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]	"right"	    "LRLLLRLLRRL" LRLLLRLLRRL
-[7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]	"left"	    "LRLLRRLLLRR" LRLLRRLLLRR
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]	    "right"	    "LLRLLRLLRL" LLRLLRLLRR
+[1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]	"right"	    "LRLLLRLLRRL"
+[7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]	"left"	    "LRLLRRLLLRR"
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]	    "right"	    "LLRLLRLLRL"
 
 
 For the first example:
